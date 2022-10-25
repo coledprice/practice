6869444c3d66056bb0e76fc9bcc0c28b5872fc02
Cole Price <colevart@gmail.com>
2022-10-25 16:44:33 -0400

    print all 3

--- store_item.rb
+++ store_item.rb
@@ -4,4 +4,6 @@ fruit_3 = {fruit: 'pear', color: 'green', price: 0.75}
 
 puts " The " + "#{fruit_2[:fruit]}" + " is the color " + "#{fruit_2[:color]}" + " and costs " + "#{fruit_2[:price]}"
 
+puts " The " + "#{fruit_3[:fruit]}" + " is the color " + "#{fruit_3[:color]}" + " and costs " + "#{fruit_3[:price]}"
 
+puts " The " + "#{fruit_1["fruit"]}" + " is the color " + "#{fruit_1["color"]}" + " and costs " + "#{fruit_1["price"]}"
