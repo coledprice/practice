695bd3dd5d84071909a4f5a98ef04aecb34d7ace
Cole Price <colevart@gmail.com>
2022-10-25 15:41:37 -0400

    print banana

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+fruit_1 = {"fruit" => 'apple', "color" => 'red', "price" => 1.00}
+fruit_2 = {fruit: 'banana', color: 'yellow', price: 0.50}
+fruit_3 = {fruit: 'pear', color: 'green', price: 0.75}
+
+puts " The " + "#{fruit_2[:fruit]}" + " is the color " + "#{fruit_2[:color]}" + " and costs " + "#{fruit_2[:price]}"
+
+
