87cda7cc7746765b1a3dfa1f8071f142456f17bb
Cole Price <colevart@gmail.com>
2022-10-27 10:39:58 -0400

    adds manager

---
+++ inheritance_example.rb
@@ -0,0 +1,38 @@
+
+class Vehicle
+  def initialize
+    @color = input_options[:color]
+    @make = input_options[:make]
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+car1 = Car.new({ color: "red", @make})
+
+
+
--- store_item.rb
+++ store_item.rb
@@ -1,11 +1,11 @@
-
 class Fruit
   attr_reader :fruit, :color, :price
   attr_writer :tax
-  def initialize(fruit, color, price)
-    @fruit = fruit
-    @color = color
-    @price = price
+ 
+  def initialize(input_options)
+    @fruit = input_options[:fruit]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 
   def print_info
@@ -15,19 +15,12 @@ class Fruit
   def tax
     @price = 0.08 + @price
   end
-
-
-
-end
-
+end 
   
 
-  fruit1 = Fruit.new("apple", "red", 1.00)
-  fruit2 = Fruit.new("banana", "yellow", 0.50)
-  fruit3 = Fruit.new("pear", "green", 0.75)
+fruit1 = Fruit.new({fruit: "apple", color: "red", price: 1.00})
+fruit2 = Fruit.new({fruit: "banana", color: "yellow", price: 0.50})
+fruit3 = Fruit.new({fruit: "pear", color: "green", price: 0.75})
 
 fruit1.print_info
 p fruit1.tax
-
-
-
